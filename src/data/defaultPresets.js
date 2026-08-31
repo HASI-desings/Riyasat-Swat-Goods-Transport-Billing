75d096e928818e84c7d3393dd5974094adffda15
HASI-desings <ranahasi302@gmail.com>
2026-09-01 02:15:25 +0500

    Add files via upload

---
+++ src/data/defaultPresets.js
@@ -0,0 +1,15 @@
+export const defaultPresets = [
+  'Cotton',
+  'Bags',
+  'Rice',
+  'Cloth Bales',
+  'Furniture',
+  'Machinery Parts',
+  'Grocery Items',
+  'Electronics',
+];
+
+export const branches = [
+  { id: 'branch-1', label: 'Branch #1', address: '32 Chowk' },
+  { id: 'branch-2', label: 'Branch #2', address: 'Shalmi Bazaar' },
+];
